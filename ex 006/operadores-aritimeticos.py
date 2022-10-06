ea4bbf471f443375235ecf5d54bdca081b42f443
Tiago Evaristo Freitas <tiago1evaristo@gmail.com>
2022-10-06 13:55:54 -0300

    operadores aritimeticos

---
+++ ex 006/operadores-aritimeticos.py
@@ -0,0 +1,5 @@
+from math import sqrt
+n = int(input('Digite um número: '))
+print(f'O dobro de {n} vale {n*2}.')
+print(f'O Triplo de {n} vale {n*3}.')
+print(f'A raiz quadrada de {n} é igual a {sqrt(n):.2f}')
