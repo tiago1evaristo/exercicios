b0d21916cc6c3205503ca315a0776db35b2d794e
Tiago Evaristo Freitas <tiago1evaristo@gmail.com>
2022-10-10 09:53:27 -0300

    calcula a dimensão e quanto de tinta sera necessário para pintar

---
+++ ex 011/pintar_parede.py
@@ -0,0 +1,4 @@
+l = float(input('Largura da parede: '))
+a = float(input('Altura da parede: '))
+print(f'Sua parede tem a dimensão de {l:.2f}x{a:.2f} e sua área é de {a*l:.2f}m².')
+print(f'Para pintar essa parede, você precisará de {(a*l)*0.5:.2f}l de tinta.')
